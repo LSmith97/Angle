b0e0d94c21260b2eef614069403d2c0ba1f40d01
jhsu79 <joshuanhsu@gmail.com>
2023-10-17 10:56:46 -0400

    initialize set-up

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+///////////////////////////////
+// DEPENDENCIES
+///////////////////////////////
+require("dotenv").config();
+require('./config/db.connection.js')
+
+const { PORT } = process.env;
+
+const express = require("express");
+const app = express();
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+

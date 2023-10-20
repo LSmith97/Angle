531f07c364492ca84545884fca7e47f1486c1dab
lsmith <therealshaunsred@gmail.com>
2023-10-20 09:56:32 -0400

    Created post containers

--- frontend/src/app/pages/index/index.component.ts
+++ frontend/src/app/pages/index/index.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Post } from '../../post';
+import { PostContainerComponent } from 'src/app/post-container/post-container.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostContainerComponent],
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
-
 export class IndexComponent {
-  testPost: Post = {
-    _id: "1",
-    title: "Test",
-    body: "Hello World",
-    isEdited: false,
-    comments: [],
-    user: "User id",
-    userName: "Username",
-  }
+  posts: Post[] = [
+    {
+      _id: '1',
+      title: 'Test',
+      body: 'Hello World',
+      isEdited: false,
+      comments: [],
+      user: 'User id',
+      userName: 'Username',
+    },
+    {
+      _id: '2',
+      title: 'Test2',
+      body: 'Hello World2',
+      isEdited: false,
+      comments: [],
+      user: 'User id2',
+      userName: 'Username2',
+    },
+  ];
 }
--- frontend/src/app/post-container/post-container.component.ts
+++ frontend/src/app/post-container/post-container.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-container',
@@ -9,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./post-container.component.css']
 })
 export class PostContainerComponent {
-
+  @Input() post!: Post;
 }

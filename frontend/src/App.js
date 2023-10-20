f193dc682350f92217bc7a0d90d4c557b50ab172
lsmith <therealshaunsred@gmail.com>
2023-10-20 18:56:32 -0400

    Created routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Main from './Components/Main';
+import Sidebar from './Components/Sidebar';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Sidebar />
+      <Main />
     </div>
   );
 }

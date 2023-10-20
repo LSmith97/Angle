340aa1365e35bcc76f65938112bbd94d6f76af39
lsmith <therealshaunsred@gmail.com>
2023-10-20 19:02:50 -0400

    Updated Header

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,7 +1,21 @@
+import "./Header.css"
+
 function Header() {
-    return(
-        <header></header>
-    )
+  return (
+    <header>
+      <a href="/">
+        <h1>Angle</h1>
+      </a>
+      <nav>
+        <a href="/">
+          <div>Home</div>
+        </a>
+        <a href="/">
+          <div>Log in</div>
+        </a>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

df5e91aa2c285d98f34f033336304908cee962d7
lsmith <therealshaunsred@gmail.com>
2023-10-20 19:13:43 -0400

    Updated layout

--- frontend/src/Pages/Details/index.js
+++ frontend/src/Pages/Details/index.js
@@ -1,5 +1,7 @@
 function Details() {
-
+    return(
+        <h1>Details</h1>
+    )
 }
 
 export default Details
--- frontend/src/Pages/Posts/index.js
+++ frontend/src/Pages/Posts/index.js
@@ -1,5 +1,7 @@
 function Posts() {
-
+    return(
+        <h1>Posts</h1>
+    )
 }
 
 export default Posts

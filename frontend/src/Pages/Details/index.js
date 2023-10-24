06832a653166dfcd4a2769bb11bf8bc51c2c5822
jhsu79 <joshuanhsu@gmail.com>
2023-10-24 15:14:21 -0400

    update details page

--- frontend/src/Pages/Details/index.js
+++ frontend/src/Pages/Details/index.js
@@ -1,13 +1,43 @@
-import PostContainer from "../../Components/PostContainer"
-import CommentList from "../../Components/CommentsList"
-import  {getOne} from "../../Utilities/post-service"
-function Details({post, comment}) {
-    return( 
-        <div>
-        <PostContainer/> 
-        <CommentList/> 
-        </div>
-    )
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getOne } from "../../Utilities/post-service";
+import PostContainer from "../../Components/PostContainer";
+import CommentList from "../../Components/CommentsList";
+import "./Details.css";
+
+function Details({ post }) {
+  const [postDetails, setPostDetails] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    handleRequest();
+  }, []);
+
+  async function handleRequest() {
+    try {
+      const postData = await getOne(id);
+      setPostDetails(postData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function loaded() {
+    return (
+      <div>
+        <PostContainer post={post} />
+        <CommentList />
+      </div>
+    );
+  }
+
+  function loading() {
+    return <h1>Loading...</h1>;
+  }
+
+  return <div>
+    {postDetails ? loaded() : loading()}
+        </div>;
 }
 
-export default Details
+export default Details;

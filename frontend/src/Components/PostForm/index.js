2cc800aae25470f9b6236b47f317dab0b7745335
lsmith <therealshaunsred@gmail.com>
2023-10-24 16:57:28 -0400

    Updated form component

--- frontend/src/Components/PostForm/index.js
+++ frontend/src/Components/PostForm/index.js
@@ -1,7 +1,34 @@
-function PostForm(){
-    return(
-        <h1>Form Works!</h1>
-    )
+function PostForm({ submit, formData, setFormData }) {
+
+  function handleChange(event) {
+    setFormData({...formData, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <form onSubmit={submit} className="post-form">
+      <input
+        id="title-input"
+        type="text"
+        name="title"
+        placeholder="Enter a title"
+        onChange={handleChange}
+        required
+      />
+
+      <textarea
+        id="body-input"
+        rows="4"
+        cols="25"
+        maxlength="500"
+        name="body"
+        placeholder=" Enter your post here"
+        onChange={handleChange}
+        required
+      ></textarea>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default PostForm
+export default PostForm;
--- frontend/src/Pages/NewPost/index.js
+++ frontend/src/Pages/NewPost/index.js
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import PostForm from "../../Components/PostForm"
 
 function NewPost() {
+    const [formData, setFormData] = useState({
+        title: "",
+        body: ""
+    })
+
     return(
-        
         <>
         <h1>New</h1>
-        <PostForm />
+        <PostForm formData={formData} setFormData={setFormData}/>
         </>
     )
 }

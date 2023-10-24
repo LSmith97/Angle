61f448d2ebb95dadd5a7ce96d0c807adb990be4c
lsmith <therealshaunsred@gmail.com>
2023-10-24 17:42:28 -0400

    Created Edit post page

--- frontend/src/Components/Main/index.js
+++ frontend/src/Components/Main/index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Posts from "../../Pages/Posts";
 import Details from "../../Pages/Details";
 import NewPost from "../../Pages/NewPost";
+import EditPost from "../../Pages/EditPost";
 
 function Main() {
   return (
@@ -10,6 +11,7 @@ function Main() {
         <Route path="/" element={<Posts />} />
         <Route path="/posts/:id" element={<Details />} />
         <Route path="/posts/new" element={<NewPost />} />
+        <Route path="/posts/:id/edit" element={<EditPost />} />
       </Routes>
     </main>
   );

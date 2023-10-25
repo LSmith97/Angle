e5d151a27857d40452ac2afae755dc9faeea82f0
lsmith <therealshaunsred@gmail.com>
2023-10-25 11:29:43 -0400

    Added user validation to comment router

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const commentsCtl = require('../controllers/comments');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.post('/posts/:id/comments', commentsCtl.create);
-router.delete('/posts/:id/comments/:commentId', commentsCtl.remove);
-router.put('/comments/:id', commentsCtl.update);
+// Create Route
+router.post('/posts/:id/comments', ensureLoggedIn, commentsCtl.create);
+
+// Delete Route
+router.delete('/posts/:id/comments/:commentId', ensureLoggedIn, commentsCtl.remove);
+
+// Update Route
+router.put('/comments/:id', ensureLoggedIn, commentsCtl.update);
 
 module.exports = router;

04d28764fa0e07580531423e636f55d442510a17
lsmith <therealshaunsred@gmail.com>
2023-10-25 12:20:14 -0400

    Updated login page layout

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,6 +1,25 @@
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+import "./Login.css"
+
 function Login() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+    }, [])
+
+    function handleSubmit() {
+
+    }
+
     return(
-        <h1>Login works!</h1>
+        <form className="login-form">
+            <h2>Log in:</h2>
+            <input type="email" name="email" placeholder="Email Address" required/>
+            <input type="password" name="password" placeholder="Password" required/>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 

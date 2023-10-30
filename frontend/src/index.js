25e3758b71c3ceb7d84f3f760887b6b9e4b29251
lsmith <therealshaunsred@gmail.com>
2023-10-30 13:29:27 -0400

    Added auth provider to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const { OAUTH_DOMAIN, OAUTH_CLIENT_ID } = process.env;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <React.StrictMode>
-      <App />
+      <Auth0Provider
+        domain={OAUTH_DOMAIN}
+        clientId={OAUTH_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </React.StrictMode>
   </Router>
 );

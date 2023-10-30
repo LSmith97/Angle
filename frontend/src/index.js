0024ab8f733852464e9b3fa37829fdb0b0468173
lsmith <therealshaunsred@gmail.com>
2023-10-30 13:47:44 -0400

    Moved auth provider to separate component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
+import Auth0ProviderWithNavigate from "./Components/Auth/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <React.StrictMode>
-      <Auth0Provider
-        domain={process.env.OAUTH_DOMAIN}
-        clientId={process.env.OAUTH_CLIENT_ID}
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
+      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithNavigate>
     </React.StrictMode>
   </Router>
 );

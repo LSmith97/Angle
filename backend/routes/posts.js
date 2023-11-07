a48c9994f79bb8d429ee5770ddf971d36d127c04
lsmith <therealshaunsred@gmail.com>
2023-11-07 10:46:55 -0500

    Stashing changes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,7 +4,6 @@ const postCtl = require('../controllers/posts');
 
 /* GET users listing. */
 router.get('/', postCtl.index);
-// router.get('/pages/:pageNum', postCtl.allPosts)
 router.get('/:id', postCtl.show);
 router.post('/', postCtl.create);
 router.delete('/:id', postCtl.remove);

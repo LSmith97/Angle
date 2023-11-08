34f95cfb4c0a485546e8782bc543f1c36a6faef0
lsmith <therealshaunsred@gmail.com>
2023-11-08 09:57:26 -0500

    Added link to edit comment page

--- frontend/src/Components/CommentContainer/index.js
+++ frontend/src/Components/CommentContainer/index.js
@@ -1,12 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import "./CommentContainer.css";
+import { Link } from "react-router-dom";
 
 function CommentContainer({ comment }) {
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-  const date = new Date(comment.createdAt)
-  const editDate = new Date(comment.editedAt)
+  const date = new Date(comment.createdAt);
+  const editDate = new Date(comment.editedAt);
 
   return (
-    <div  className="comment-container">
+    <div className="comment-container">
       <div className="comment-header">
         <img
           className="h-10 w-10 rounded-full"
@@ -17,6 +20,11 @@ function CommentContainer({ comment }) {
       </div>
       <p className="comment-body">{comment.body}</p>
       <div className="comment-footer">
+        {isAuthenticated && user.sub === comment.user.sub ? (
+          <Link to={`/comments/${comment._id}/edit`}>
+            <p>Edit Comment</p>
+          </Link>
+        ) : null}
         {comment.isEdited ? (
           <p> Edited on {editDate.toDateString}</p>
         ) : (
